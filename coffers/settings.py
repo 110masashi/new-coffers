8b27b630562939ce7a8b5cd60417a52b442d4887
masa <74949573+110masashi@users.noreply.github.com>
2021-07-28 17:26:26 +0900

    デプロイの為データベースの設定を変更

--- coffers/settings.py
+++ coffers/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'coffers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coffers_db',
+        'USER': 'db_itoumasashi',
+        'PASSWORD': 'mark34masa',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
